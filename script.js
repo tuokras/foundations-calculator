0fdd5858aa135f1046f548c6341654b1ef2d9ba1
tuokras <tuokras@gmail.com>
2023-01-04 20:53:12 +0200

    Add html buttons

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
     return a / b;
 };
+
+const operate = function (operator, a, b) {
+    if (operator === "add") {
+        return add(a, b);
+    } else if (operator === "subtract") {
+        return subtract(a, b);
+    } else if (operator === "multiply") {
+        return multiply(a, b);
+    } else if (operator === "divide") {
+        return subtract(a, b);
+    }
+
+};
