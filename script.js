25293123152b3628f28fd8f7a15559c9fa11a326
tuokras <tuokras@gmail.com>
2023-01-08 09:32:55 +0200

    Begin code restructuring

--- script.js
+++ script.js
@@ -31,64 +31,80 @@ const operate = function (operator, a, b) {
     }
 }
 
-let display = "";
-let a = 0;
-let b = 0;
-let operator;
+let currentDisplayArray = [];
+let previousResult = 0;
+const listOfOperators = ["add", "subtract", "multiply", "divide"];
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        const buttonType = event.target.dataset.type;
-        console.log(buttonType);
+const numberButtons = document.querySelectorAll("[data-type='number']");
+const operatorButtons = document.querySelectorAll("[data-type='operator']");
+const equalsButton = document.querySelector("[data-type='equals']");
+const allClearButton = document.querySelector("[data-type='all-clear']");
+const deleteButton = document.querySelector("[data-type='delete']");
+const dotButton = document.querySelector("[data-type='dot']");
 
-        let operation = false;
-
-        //button type
-        switch (buttonType) {
-            case "number":
-                if (event.target.id === "0") {
-                    display = event.target.id;
-                } else {
-                    display += event.target.id;
-                }
-                break;
-            case "operator":
-                a = Number(display);
-                console.log("a: " + a);
-                operation = true;
-                operator = event.target.id;
-                console.log(operator);
-                break;
-            case "equals":
-                b = Number(display);
-                console.log("b: " + b);
-                display = operate(operator, a, b);
-                console.log(display);
-                break;
-            case "all-clear":
-                display = "";
-                a = 0;
-                b = 0;
-                break;
-            default:
-                console.log("ERROR IN BUTTON TYPE EVALUATOR")
-                break;
-        }
-
-        //display handling
-        switch (operation) {
-            case false:
-                document.querySelector(".display").textContent = display;
-                break;
-            case true:
-                document.querySelector(".display").textContent = a;
-                display = "";
-                break;
-        }
-
-        if (display === "" && operation === false) {
-            document.querySelector(".display").textContent = "0";
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", (e) => {
+        let numberId = e.target.id;
+        currentDisplayArray.push(numberId);
+        console.log(currentDisplayArray);
+        if (findOperatorIndex(currentDisplayArray) === 0) {
+            console.log("evaluating after pressing number");
+            evaluate(currentDisplayArray);
         }
     })
 })
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", (e) => {
+        let operatorId = e.target.id;
+        //check whether the current operator is already pressed
+        if (currentDisplayArray[currentDisplayArray.length - 1] === operatorId) return;
+        //check whether any of the other operators is selected
+        if (findOperatorIndex(currentDisplayArray) === (currentDisplayArray.length - 1)) {
+            
+        }
+        if (listOfOperators.some(r => currentDisplayArray.includes(r))) {
+            //evaluate first, add operator after previous result
+            console.log("operator id already added, evaluating");
+            evaluate(currentDisplayArray);
+            currentDisplayArray.push(operatorId);
+            console.log("added operator id");
+        } else {
+            currentDisplayArray.push(operatorId);
+            console.log("added operator id");
+        }
+    })
+})
+
+const evaluate = function (array) {
+    //find index of operator
+    //split array to two parts, before and after the operator
+    //turn array to number
+    //calculate result
+    let operatorIndex = findOperatorIndex(currentDisplayArray);
+    console.log("operator index: " + operatorIndex)
+
+    let a = arrayToNumber(currentDisplayArray.slice(0, operatorIndex));
+    let b = arrayToNumber(currentDisplayArray.slice(operatorIndex + 1));
+    console.log(a);
+    console.log(b);
+    let result = previousResult + operate(currentDisplayArray[operatorIndex], a, b);
+    console.log(result);
+    clear();
+}
+
+const findOperatorIndex = function (array) {
+    for (currentOperator of listOfOperators) {
+        if (array.indexOf(currentOperator) > -1) {
+            return array.indexOf(currentOperator);
+        }
+    }
+}
+
+const clear = function () {
+    currentDisplayArray = [];
+}
+
+const arrayToNumber = function (array) {
+    return array.map(Number).reduce((accum, digit) => (accum * 10) + digit, 0);
+}
